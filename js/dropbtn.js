2e045c875d1f636946ecfaffac800bc334af33e6
Sonika Prakash <sonikaprakash10@gmail.com>
2021-07-17 20:02:26 +0530

    Add files via upload

---
+++ js/dropbtn.js
@@ -0,0 +1,25 @@
+function drop()
+{
+    dd = document.getElementById("drop");
+    if(dd.classList.contains("show"))
+    {
+        dd.classList.remove("show");
+    }
+    else
+    {
+        dd.classList.add("show");
+    }
+}
+
+function drop1()
+{
+    dd1 = document.getElementById("drop1");
+    if(dd1.classList.contains("show"))
+    {
+        dd1.classList.remove("show");
+    }
+    else
+    {
+        dd1.classList.add("show");
+    }
+}
